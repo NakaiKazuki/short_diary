3dc521d41eec7175728c5a38c2e2e6e7a0accb28
Kazuk Nakai <na.kazuki08@gmail.com>
2020-11-06 05:11:37 +0900

    docker編集

---
+++ config/puma.rb
@@ -0,0 +1,20 @@
+workers Integer(ENV['WEB_CONCURRENCY'] || 2)
+threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
+threads threads_count, threads_count
+
+preload_app!
+
+rackup      DefaultRackup
+port        ENV['PORT']     || 3000
+environment ENV['RACK_ENV'] || 'development'
+
+on_worker_boot do
+ActiveRecord::Base.establish_connection
+end
+
+plugin :tmp_restart
+
+app_dir = File.expand_path("../..", __FILE__)
+bind "unix://#{app_dir}/tmp/sockets/puma.sock"
+pidfile "#{app_dir}/tmp/pids/puma.pid"
+stdout_redirect "#{app_dir}/log/puma.stdout.log", "#{app_dir}/log/puma.stderr.log", true
