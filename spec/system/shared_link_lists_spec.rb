2a04f47b058cce0a7585e97362e66770afcc165d
Kazuki Nakai <na.kazuki08@gmail.com>
2020-12-04 15:26:06 +0900

    link_list 完成

---
+++ spec/system/shared_link_lists_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe "SharedLinkLists", type: :system do
+  let(:user) { create(:user) }
+
+  describe "shared/_link_list layout" do
+    before do
+      sign_in user
+      visit root_path
+    end
+
+    it "登録情報編集ページへのリンクがある" do
+      expect(page).to have_link "登録情報編集" , href: edit_user_registration_path
+    end
+  end
+end
