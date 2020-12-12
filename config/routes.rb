916ae8649c64443e3d6b17590260991bec4a6144
Kazuki Nakai <na.kazuki08@gmail.com>
2020-12-12 23:46:15 +0900

    フォーム失敗後のページ再表示エラー対策追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
-  root "static_pages#home"
+  root 'static_pages#home'
+  get 'users', to: 'static_pages#home'
+  get 'microposts', to: 'static_pages#home'
   devise_for :users, controllers: {
     confirmations: 'users/confirmations',
-    registrations: 'users/registrations' }
+    registrations: 'users/registrations'
+  }
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#new_guest'
   end
-  resources :microposts, only: [:create, :destroy, :index]
+  resources :microposts, only: %i[create destroy]
 end
