669afaec4f03b45454fd6f8a7eaf81dd37075530
Kazuki Nakai <na.kazuki08@gmail.com>
2020-12-15 02:55:52 +0900

    微調整

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,9 @@
 User.create!(email: "user@example.com",
              password: "password",
              password_confirmation: "password",
-             confirmed_at: Date.today)
+             confirmed_at: Time.current)
 
 User.create!(email: "guest@example.com",
              password: "password",
              password_confirmation: "password",
-             confirmed_at: Date.today)
+             confirmed_at: Time.current)
