5466fcd3382ab36ff55736fff4fd35ad5b3b24b0
Kazuki Nakai <na.kazuki08@gmail.com>
2021-01-13 07:18:42 +0900

    修正

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,6 +3,9 @@ server '52.193.58.137', user: 'Kazuki', roles: %w[web app db]
 set :deploy_to, '/var/www/rails/short_diary'
 
 set :ssh_options, {
-  keys: %w[~/.ssh/short_diary_key_rsa],
+  keys: %w[~/.ssh/ec2_key_rsa],
   forward_agent: true
 }
+
+# デプロイ時はssh-agent追加する
+# eval `ssh-agent` && ssh-add ~/.ssh/git_rsa
--- entrypoint.sh
+++ entrypoint.sh
@@ -3,13 +3,9 @@
 mkdir -p ~/.ssh
 chown -R root:root ~/.ssh
 chmod -R 700 ~/.ssh
-cp -ip /run/secrets/ec2_ssh_key ~/.ssh/short_diary_key_rsa
-cp -ip /run/secrets/git_hub_ssh_key ~/.ssh/ec2_git_rsa
-chmod -R 600 ~/.ssh/ec2_git_rsa
-chmod -R 400 ~/.ssh/short_diary_key_rsa
-# これを自動で行うようにする or Capistranoのファイルで読み取らせる のが課題
-# eval `ssh-agent`
-#
-# ssh-add ~/.ssh/ec2_git_rsa
+cp -ip /run/secrets/ec2_key ~/.ssh/ec2_key_rsa
+cp -ip /run/secrets/git_key ~/.ssh/git_rsa
+chmod -R 400 ~/.ssh/ec2_key_rsa
+chmod -R 400 ~/.ssh/git_rsa
 
 exec "$@"
