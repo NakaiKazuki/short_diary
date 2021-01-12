008dee8ab396a61a9e655c65ab40a868ae78bd03
Kazuki Nakai <na.kazuki08@gmail.com>
2021-01-13 04:45:40 +0900

    保存

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,14 +7,14 @@ set :ssh_options, {
   forward_agent: true
 }
 
-# ソケットの場所、Nginxとのやり取りに必要
-set :puma_bind, "unix://#{shared_path}/tmp/sockets/puma.sock"
-# サーバー状態を表すファイルの場所
-set :puma_state, "#{shared_path}/tmp/pids/puma.state"
-# プロセスを表すファイルの場所
-set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
-# ログの場所
-set :puma_access_log, "#{shared_path}/log/puma.error.log"
-set :puma_error_log, "#{shared_path}/log/puma.access.log"
-# puma
-set :puma_init_active_record, true
+# # ソケットの場所、Nginxとのやり取りに必要
+# set :puma_bind, "unix://#{shared_path}/tmp/sockets/puma.sock"
+# # サーバー状態を表すファイルの場所
+# set :puma_state, "#{shared_path}/tmp/pids/puma.state"
+# # プロセスを表すファイルの場所
+# set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
+# # ログの場所
+# set :puma_access_log, "#{shared_path}/log/puma.error.log"
+# set :puma_error_log, "#{shared_path}/log/puma.access.log"
+# # puma
+# set :puma_init_active_record, true
