f6cf7e2c5fc07b785fcbcfaff78e4221eaf71d05
Kazuki Nakai <na.kazuki08@gmail.com>
2021-01-17 03:47:22 +0900

    修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,7 +70,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: Rails.application.credentials.amazon[:ec2_ip] }
+  config.action_mailer.default_url_options = { host: Rails.application.credentials.host_server[:name] }
   ActionMailer::Base.smtp_settings = {
     address: 'smtp.gmail.com',
     port: '587',
