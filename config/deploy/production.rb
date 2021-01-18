3b0c409bb33b62d2d20a9536a87f9c40dd5b4494
Kazuki Nakai <na.kazuki08@gmail.com>
2021-01-19 06:28:20 +0900

    修正

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,13 +1,13 @@
 set :rails_env, :production
 
-server Rails.application.credentials.dig(:amazon, :ec2_ip),
+server Rails.application.credentials.amazon.dig(:amazon, :ec2_ip),
        user: Rails.application.credentials.dig(:amazon, :ec2_user),
        roles: %w[web app db]
 
 set :deploy_to, '/var/www/rails/short_diary'
 
 set :ssh_options, {
-  keys: %w[~/.ssh/short_diary_key_rsa],
+  keys: [ENV.fetch('PRODUCTION_SSH_KEY').to_s],
   forward_agent: true
 }
 
