030747858f9e8b776f9327d7d28e15ddd2c592fd
Kazuki Nakai <na.kazuki08@gmail.com>
2021-01-19 05:11:37 +0900

    circleci capistrano自動デプロイ追加

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,7 +70,6 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  .dig(:host_server, :name)
   config.action_mailer.default_url_options = { host: Rails.application.credentials.dig(:host_server, :name) }
   ActionMailer::Base.smtp_settings = {
     address: 'smtp.gmail.com',
