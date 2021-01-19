c17767b14d958dd77a322df06d43ab5c7f1f7661
Kazuki Nakai <na.kazuki08@gmail.com>
2021-01-20 04:52:07 +0900

    修正

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+set -ex
+
+IP=`curl -f -s ifconfig.me`
+
+trap "aws ec2 revoke-security-group-ingress --group-id ${SECURITY_GROUP_ID} --protocol tcp --port 22 --cidr ${IP}/32" 0 1 2 3 15
+aws ec2 authorize-security-group-ingress --group-id ${SECURITY_GROUP_ID} --protocol tcp --port 22 --cidr ${IP}/32
+bundle exec cap production deploy
