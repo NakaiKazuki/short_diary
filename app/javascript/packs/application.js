7df26538eb0e1f76e9f8de005774fc5b671d31bc
Kazuki Nakai <na.kazuki08@gmail.com>
2021-02-13 20:40:25 +0900

    画面上部に表示されるフラッシュメッセージを表示から4秒で自動削除に変更

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,4 +15,5 @@ require("bootstrap")
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 import '../src/application.scss';
+import '../src/application.js';
 window.$ = jQuery;
